005e74d0ae85ef7b77109bfb7fca0e35a9c32f32
JoseYe628 <171573@unsaac.edu.pe>
2024-05-09 18:11:49 +0000

    CustomBottomBar: refactoring, moviendo las animaciones en custom_botton_navigation_bar

--- lib/widgets/custom_bottom_navigation_bar/custom_bottom_navigation_bar.dart
+++ lib/widgets/custom_bottom_navigation_bar/custom_bottom_navigation_bar.dart
@@ -2,8 +2,37 @@
 import 'package:flutter/material.dart';
 import 'package:productivity_app/widgets/custom_bottom_navigation_bar/widgets/widgets.dart';
 
-class CustomBottomNavigationBar extends StatelessWidget {
+class CustomBottomNavigationBar extends StatefulWidget {
   const CustomBottomNavigationBar({super.key});
+
+  @override
+  State<CustomBottomNavigationBar> createState() => _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> with TickerProviderStateMixin {
+
+  late AnimationController controller;
+  late Animation<double> izquierda;
+  double val = 110;
+
+  @override
+  void initState() {
+    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 900));
+    izquierda = Tween(begin: 110.0, end: 290.0).animate(CurvedAnimation(parent: controller, curve: Interval(0,1, curve: Curves.easeIn)));
+    controller.addListener((){
+      setState(() {
+        val = izquierda.value;
+      });
+    });
+    controller.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context){
@@ -11,10 +40,10 @@ class CustomBottomNavigationBar extends StatelessWidget {
       //height: 60,
       padding: const EdgeInsets.symmetric(vertical: 5),
       color: Colors.blueAccent,
-      child: const Column(
+      child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Row(
+          const Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               CustomBottomNavigationItem(),
@@ -22,7 +51,7 @@ class CustomBottomNavigationBar extends StatelessWidget {
               CustomBottomNavigationItem(),
             ],
           ),
-          SnackNavigation()
+          SnackNavigation(point: val,)
         ],
       ),
     );
--- lib/widgets/custom_bottom_navigation_bar/widgets/snack_navigation/snak_navigation.dart
+++ lib/widgets/custom_bottom_navigation_bar/widgets/snack_navigation/snak_navigation.dart
@@ -2,41 +2,10 @@
 import 'package:flutter/material.dart';
 import 'package:productivity_app/widgets/custom_bottom_navigation_bar/widgets/snack_navigation/snack_painter.dart';
 
-class SnackNavigation extends StatefulWidget {
-  const SnackNavigation({super.key});
+class SnackNavigation extends StatelessWidget {
+  SnackNavigation({super.key, required this.point});
 
-  @override
-  State<SnackNavigation> createState() => _SnackNavigationState();
-}
-
-class _SnackNavigationState extends State<SnackNavigation> with TickerProviderStateMixin {
-
-  late AnimationController _controller;
-  late Animation<double> izquierda;
-
-  double pointB = 110;
-
-  @override
-  void initState() {
-    _controller = AnimationController(duration: Duration(milliseconds: 1000), vsync: this);
-    izquierda = Tween(begin: 110.0, end: 290.0).animate(CurvedAnimation(parent: _controller, curve: Interval(0,1, curve: Curves.easeIn)));
-    _controller.addListener((){
-      //print(_controller.value);
-      setState(() {
-        pointB = izquierda.value;
-      });
-      print(izquierda.value);
-    });
-    _controller.forward();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+  double point;
 
   @override
   Widget build(BuildContext context){
@@ -45,7 +14,7 @@ class _SnackNavigationState extends State<SnackNavigation> with TickerProviderSt
       color: Colors.red,
       width: MediaQuery.of(context).size.width,
       child: CustomPaint(
-        painter: SnackPainter(pointA: pointB - 100 ,pointB: pointB),
+        painter: SnackPainter(pointA: point - 100 ,pointB: point),
       ),
     );
   }
